a2cbe6f2d4a0475ac6b6a2a47a49a9f43d03413b
Martin Ve <martinvetsu@gmail.com>
2026-07-06 06:19:20 +0200

    chore: kafka 1st stage

---
+++ src/main/kotlin/cz/martinvedra/triagebe2/kafka/KafkaConfig.kt
@@ -0,0 +1,29 @@
+package cz.martinvedra.triagebe2.kafka
+
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+
+@Configuration
+class KafkaConfig(
+    @Value("\${spring.kafka.bootstrap-servers}")
+    private val bootstrapServers: String
+) {
+
+    @Bean
+    fun producerFactory(): ProducerFactory<String, String> {
+        val props: MutableMap<String, Any> = HashMap()
+        props[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers
+        props[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        props[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        return DefaultKafkaProducerFactory(props)
+    }
+
+    @Bean
+    fun kafkaTemplate(): KafkaTemplate<String, String> = KafkaTemplate(producerFactory())
+}
--- src/main/kotlin/cz/martinvedra/triagebe2/service/csasrates/ExchangeRateServiceImpl.kt
+++ src/main/kotlin/cz/martinvedra/triagebe2/service/csasrates/ExchangeRateServiceImpl.kt
@@ -2,13 +2,25 @@ package cz.martinvedra.triagebe2.service.csasrates
 
 import cz.martinvedra.triagebe2.api.csasrates.dto.ExchangeRateDto
 import cz.martinvedra.triagebe2.client.ExchangeRateClient
+import cz.martinvedra.triagebe2.kafka.KafkaProducerService
 import org.springframework.stereotype.Service
 
 @Service
 class ExchangeRateServiceImpl(
-    private val restClient: ExchangeRateClient
+    private val restClient: ExchangeRateClient,
+    private val kafkaProducerService: KafkaProducerService
 ) : ExchangeRateService {
+
     override fun getExchangeRates(): List<ExchangeRateDto> {
         return restClient.getRates()
+    }
+
+    override fun publishRates(): Int {
+        val rates = getExchangeRates()
+        rates.forEach { rate ->
+            val key = rate.shortName ?: "n/a"
+            kafkaProducerService.sendRate(key, rate)
+        }
+        return rates.size
     }
 }
